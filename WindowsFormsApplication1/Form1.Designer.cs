331cac5c07e609aafbff2f8b0ae6e7782b9ac7cc
Mika <Mika@DESKTOP-JEEQNL0>
2020-06-29 00:54:32 +0800

    本地代码上传

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,48 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 221);
+            this.button1.Location = new System.Drawing.Point(17, 45);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 32);
+            this.button1.Size = new System.Drawing.Size(83, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "日文";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(54, 21);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(447, 20);
-            this.comboBox1.TabIndex = 1;
-            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(54, 60);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 81);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.MaximumSize = new System.Drawing.Size(554, 589);
+            this.richTextBox1.MinimumSize = new System.Drawing.Size(554, 589);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(447, 142);
+            this.richTextBox1.Size = new System.Drawing.Size(554, 589);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(521, 25);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 45);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Unicode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(311, 45);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "中文";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(455, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "翻译";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AcceptButton = this.button1;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 344);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(554, 670);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(570, 709);
+            this.MinimumSize = new System.Drawing.Size(570, 709);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "编码转换";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragEnter);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragEnter);
             this.ResumeLayout(false);
 
         }
@@ -78,7 +141,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,12 +22,150 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //string wb = System.IO.File.ReadAllText(comboBox1.Text);
+            //string bjk = Encoding.Unicode.GetString(Encoding.GetEncoding(wb).GetBytes(wb));
+            //  byte[] zf;
+            //  zf= System.Text.Encoding.Convert(Encoding.GetEncoding(wb), Encoding.GetEncoding("GB2312"), Encoding.GetEncoding(wb).GetBytes(wb));
+            //  richTextBox1.Text = Encoding.Unicode.GetString(zf); 
+            // richTextBox1.Text = System.IO.File.ReadAllText(comboBox1.Text);
+            if (comboBox1.Text != "")
+            {
+                string 文本 = System.IO.File.ReadAllText(comboBox1.Text);
+                byte[] 整数文本 = System.IO.File.ReadAllBytes(comboBox1.Text);
+                string 转码文本 = Encoding.GetEncoding("shift_jis").GetString(整数文本);
+                richTextBox1.Text = 转码文本;
+            }
+            else
+            {
+                MessageBox.Show("没有输入任何文本文档");
+
+
+            }
             
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Form3 form = new Form3();
+            // form.Show();
+            //this.richTextBox1.AllowDrop = true;
+
+            
 
         }
+
+        private void comboBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            string 获取的目录 = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            comboBox1.Text = 获取的目录;
+            comboBox1.Items.Add(获取的目录);
+            button2_Click(sender, e);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string 文本="";
+            if (comboBox1.Text == "")
+            {
+
+                if (richTextBox1.Text =="")
+                {
+                    MessageBox.Show("没有输入任何字符和文本文件");
+
+                }
+
+               else
+                {
+                   // 文本 = System.IO.File.ReadAllText(richTextBox1.Text);
+                }
+
+
+
+            }
+           else
+            {
+
+                文本 = System.IO.File.ReadAllText(comboBox1.Text);
+                 richTextBox1.Text = 文本;
+
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            if (comboBox1.Text != "")
+            {
+                string 文本 = System.IO.File.ReadAllText(comboBox1.Text);
+                byte[] 整数文本 = System.IO.File.ReadAllBytes(comboBox1.Text);
+                string 转码文本 = Encoding.GetEncoding("gb2312").GetString(整数文本);
+                richTextBox1.Text = 转码文本;
+            }
+            else
+            {
+                MessageBox.Show("没有输入任何文本文档");
+
+
+            }
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form3 form = new Form3();
+            form.Show();
+        }
+
+
+
+       //private void richTextBox1_DragEnter(object sender, DragEventArgs e)
+       //{
+       //     if (e.Data.GetDataPresent(DataFormats.FileDrop))
+       //         e.Effect = DragDropEffects.All;
+
+       // }
+
+
+
+
+
+
+        
+        //private void richTextBox1_DragDrop(object sender, DragEventArgs e)
+        //{
+        //    if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        //    {
+
+        //        string[] MyPathArray =(string[])e.Data.GetData(DataFormats.FileDrop);
+        //        foreach (string MyPath in MyPathArray)
+        //        {
+        //            this.richTextBox1.LoadFile(MyPath, RichTextBoxStreamType.PlainText);
+        //        }
+        //    }
+
+        //}
+        
+
     }
 }
